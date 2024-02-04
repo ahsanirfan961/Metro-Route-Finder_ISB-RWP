12f9fc2eca223aa3b236b64ddff580441380dba8
Muhammad Ahsan <ahsanirfan961@gmail.com>
2024-02-04 23:16:33 +0500

    4 January 2024

--- lib/bottom_sheet.dart
+++ lib/bottom_sheet.dart
@@ -25,31 +25,44 @@ class MainBottomSheet extends StatelessWidget {
       child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             RadioMenuButton(
-                value: true,
-                groupValue: true,
-                onChanged: (bool? value) {},
-                style: ButtonStyle(
-                  shape: MaterialStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set the border radius
-                    ),
+              value: true,
+              groupValue: true,
+              onChanged: (bool? value) {
+                if (value == true) {
+                  showModalBottomSheet(
+                    context: context as BuildContext,
+                    builder: (BuildContext context) => SearchBottomSheet(),
+                  );
+                }
+              },
+              style: ButtonStyle(
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
                 ),
-                child: const Text('Start Location')),
+              ),
+              child: const Text('Start Location'),
+            ),
             const Divider(),
             RadioMenuButton(
                 value: true,
                 groupValue: true,
-                onChanged: (bool? value) {},
+                onChanged: (bool? value) {
+                  if (value == true) {
+                    showModalBottomSheet(
+                      context: context as BuildContext,
+                      builder: (BuildContext context) => SearchBottomSheet(),
+                    );
+                  }
+                },
                 style: ButtonStyle(
                   shape: MaterialStatePropertyAll(
                     RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set the border radius
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
                 ),
@@ -58,22 +71,58 @@ class MainBottomSheet extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              style: const ButtonStyle(
-                backgroundColor:
-                    MaterialStatePropertyAll(Colors.deepOrangeAccent),
-                elevation: MaterialStatePropertyAll(3),
-              ),
-              child: const Text(
-                'Find Route',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  // fixedSize: Size(width, height),
+                  backgroundColor:
+                      MaterialStatePropertyAll(Colors.deepOrangeAccent),
+                  elevation: MaterialStatePropertyAll(3),
+                ),
+                child: const Text(
+                  'Find Route',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             )
           ]),
+    );
+  }
+}
+
+class SearchBottomSheet extends StatelessWidget {
+  final searchController = TextEditingController();
+
+  SearchBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 600,
+      child: Column(children: [
+        Container(
+          width: MediaQuery.of(context).size.width * (1 / 2),
+          height: 5,
+          margin: const EdgeInsets.only(top: 5),
+          decoration: const BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.all(Radius.circular(2.5))),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: TextField(
+            controller: searchController,
+            decoration: const InputDecoration(
+                hintText: 'Search Location', icon: Icon(Icons.search)),
+          ),
+        )
+      ]),
     );
   }
 }
